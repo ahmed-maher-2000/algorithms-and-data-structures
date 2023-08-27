50989956b54adccf4941c8baa4176a842f4f3bcc
Ahmed Maher <ahmededu015@gmail.com>
2023-08-27 11:20:37 +0300

    fix selection sort

--- Algortithms/Sort/selectionSort.js
+++ Algortithms/Sort/selectionSort.js
@@ -1,10 +1,13 @@
 function selectionSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        [arr[j], arr[i]] = [arr[i], arr[j]];
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
       }
     }
+
+    if (minIndex !== i) [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
   }
 
   return arr;
